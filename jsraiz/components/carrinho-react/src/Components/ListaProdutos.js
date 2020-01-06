9e381fb823e702accd17a9847f52383c2d63b393
fbalbino <fjbalbino@gmail.com>
2020-01-05 21:27:51 -0300

    first commit

---
+++ jsraiz/components/carrinho-react/src/Components/ListaProdutos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ListaProdutosComponent(props) { 
+    return (
+        <div className="row loja">
+        {props.children}  
+        </div>
+    )
+}
